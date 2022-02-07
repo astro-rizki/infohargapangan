e7522808058302bba545c6f0fc34e5e617f1731c
Rizki Afrizal <srizkiafrizal85@gmail.com>
2022-02-07 16:06:43 +0700

    Update poster.py

--- poster.py
+++ poster.py
@@ -5,22 +5,26 @@ from requests.exceptions import HTTPError
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET"))
 auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
-
 # Create API object
 api = tweepy.API(auth)
 
-harga = 0
+CATEGORY = ["cat-1", "cat-2", "cat-3", "cat-4", "cat-5", "cat-6", "cat-7", "cat-8", "cat-9", "cat-10"]
+CATEGORY_NAME = ["Beras", "Daging Ayam", "Daging Sapi", "Telur Ayam", "Bawang Merah", "Bawang Putih", "Cabai Merah", "Cabai Rawit", "Minyak Goreng", "Gula Pasir"]
+DATE = datetime.datetime.now()
+DATE = DATE.strftime("%d-%m-%Y")
+
 try:
-    response = requests.get('https://jibs.my.id/api/harga_komoditas')
-    response.raise_for_status()
-    # access JSOn content
-    jsonResponse = response.json()
-    print("Entire JSON response")
-    print(type(jsonResponse["national_commodity_price"]["Daging Ayam"]))
+    for i in range(0, len(CATEGORY)):
+        print(i)
+        como = requests.get('https://hargapangan.id/index.php?option=com_gtpihps&price_type_id=1&data_type=price&date='+DATE+'&task=json.getData&commodity_id='+CATEGORY[i])
+        como.raise_for_status()
+        como = como.json()["tableData"]
+        como = json_normalize(como)
+        como.sort_values('value')
+        # Create a tweet
+        api.update_status(CATEGORY_NAME[i])
+    
 except HTTPError as http_err:
     print(f'HTTP error occurred: {http_err}')
 except Exception as err:
     print(f'Other error occurred: {err}')
-
-# Create a tweet
-# api.update_status("Hello Tweepy")
