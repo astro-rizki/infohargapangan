d2a4460ec781d601b069630e8e2c611d83595cfe
Rizki Afrizal <srizkiafrizal85@gmail.com>
2022-02-08 19:04:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
+import tweepy
+import requests
+import datetime
+from requests.exceptions import HTTPError
+from pandas import json_normalize
 from flask import Flask
-from datetime import datetime
+  
 app = Flask(__name__)
 
+auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET"))
+auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
+api = tweepy.API(auth)
+
+CATEGORY = ["cat-1", "cat-2", "cat-3", "cat-4", "cat-5", "cat-6", "cat-7", "cat-8", "cat-9", "cat-10"]
+CATEGORY_NAME = ["Beras", "Daging Ayam", "Daging Sapi", "Telur Ayam", "Bawang Merah", "Bawang Putih", "Cabai Merah", "Cabai Rawit", "Minyak Goreng", "Gula Pasir"]
+DATE = datetime.datetime.now()
+DATE = DATE.strftime("%d-%m-%Y")
+  
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
+def hello_world():
+    # try:
+    #     for i in range(0, len(CATEGORY)):
+    #         print(i)
+    #         como = requests.get('https://hargapangan.id/index.php?option=com_gtpihps&price_type_id=1&data_type=price&date='+DATE+'&task=json.getData&commodity_id='+CATEGORY[i])
+    #         como.raise_for_status()
+    #         como = como.json()["tableData"]
+    #         como = json_normalize(como)
+    #         como.sort_values('value')
+    #         # Create a tweet
+    #         api.update_status(CATEGORY_NAME[i])
+        
+    # except HTTPError as http_err:
+    #     print(f'HTTP error occurred: {http_err}')
+    # except Exception as err:
+    #     print(f'Other error occurred: {err}')
+    return 'OK'
+  
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 
